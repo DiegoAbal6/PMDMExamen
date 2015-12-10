41c6add5c2429586482195623d1de618feba455b
DiegoAbal6 <diegoabalam@gmail.com>
2015-12-10 19:24:51 +0100

    Final.
    Código comentado en los puntos consideras importantes

--- app/src/main/java/com/example/maripes/listdetail/DriverListActivity.java
+++ app/src/main/java/com/example/maripes/listdetail/DriverListActivity.java
@@ -64,7 +64,7 @@ public class DriverListActivity extends AppCompatActivity
             // large-screen layouts (res/values-large and
             // res/values-sw600dp). If this view is present, then the
             // activity should be in two-pane mode.
-            mTwoPane = true; 
+            mTwoPane = true;
 
             // In two-pane mode, list items should be given the
             // 'activated' state when touched.
